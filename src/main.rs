477e8513670cde00b96693e31647998cee0c4eeb
br-netw <vanmain.22@gmail.com>
2022-11-06 20:13:26 +0300

    Сменил шрифты, перевел интерфейс

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use iced::alignment::Horizontal;
 fn main() -> iced::Result {
     let mut gm_settings = Settings::default();
     gm_settings.id = Some(String::from("grademapper-rs"));
-    // Шрифт не меняется. Совсем. TODO
-    //gm_settings.default_font = Some(b"/usr/share/fonts/TTF/JetBrainsMono-Regular.ttf"); 
+    // Font fallback'a в iced нет. Страдайте.
+    gm_settings.default_font = Some(include_bytes!("/usr/share/fonts/TTF/OpenSans-Regular.ttf")); 
     GradeMapper::run(gm_settings)
 }
 
@@ -16,6 +16,7 @@ enum WorkType {
     Classwork,
     Test1,
     Test2,
+    VocalExam,
     Exam
 }
 
@@ -67,7 +68,7 @@ impl Application for GradeMapper {
             Message::ProcessGrade => {
                 if let Ok(res) = self.current_grade.parse() {
                     if res > 5 || res < 1 { // проверка на валидность оценки
-                        self.avg = String::from("ERROR: incorrect grade");
+                        self.avg = String::from("ОШИБКА: неверная оценка");
                     } else {
                         self.grades.push(res);
                         self.weights.push(self.current_weight);
@@ -76,7 +77,7 @@ impl Application for GradeMapper {
                     self.work_type_selected = Some(WorkType::Classwork);
                     self.current_weight = 1.0;
                 } else {
-                    self.avg = String::from("ERROR: not a number"); 
+                    self.avg = String::from("ОШИБКА: не является числом"); 
                 }
                 self.current_grade = String::new();
             },
@@ -85,6 +86,7 @@ impl Application for GradeMapper {
                     WorkType::Classwork => self.current_weight = 1.0,
                     WorkType::Test1 => self.current_weight = 1.2,
                     WorkType::Test2 => self.current_weight = 1.3,
+                    WorkType::VocalExam => self.current_weight = 1.4,
                     WorkType::Exam => self.current_weight = 1.5,
                 };
                 self.work_type_selected = Some(t);
@@ -100,32 +102,34 @@ impl Application for GradeMapper {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        let avg_out = Text::new(format!("Average grade: {}", self.avg))
+        let avg_out = Text::new(format!("Средний балл: {}", self.avg))
             .horizontal_alignment(Horizontal::Center);
 
-        // кириллица не работает
+        // Вероятно, кнопки центрируются через .style(), но разбираться в нём мне сейчас лень
         let submit_button = Button::new(&mut self.submit_button_state, 
-                                        Text::new("Submit").horizontal_alignment(Horizontal::Center))
+                                        Text::new("Рассчитать").horizontal_alignment(Horizontal::Center))
             .on_press(Message::ProcessGrade).width(FillPortion(1)).padding(10);
         let rm_button = Button::new(&mut self.remove_button_state, 
-                                    Text::new("Delete last grade").horizontal_alignment(Horizontal::Center))
+                                    Text::new("Удалить прошлую оценку").horizontal_alignment(Horizontal::Center))
             .on_press(Message::RemoveGrade).width(FillPortion(1)).padding(10);
-        let grade_in = TextInput::new(&mut self.text_inp_state, "Enter grade", &self.current_grade, Message::EditGrade)
+        let grade_in = TextInput::new(&mut self.text_inp_state, "Введите оценку", &self.current_grade, Message::EditGrade)
             .padding(10);
 
-        let type_classwork = Radio::new(WorkType::Classwork, "Klassanaya", self.work_type_selected, Message::WorkTypeChoice);
-        let type_test1 = Radio::new(WorkType::Test1, "Samostoyatelnaya", self.work_type_selected, Message::WorkTypeChoice);
-        let type_test2 = Radio::new(WorkType::Test2, "Proverochnaya", self.work_type_selected, Message::WorkTypeChoice);
-        let type_exam = Radio::new(WorkType::Exam, "Kontrolnaya", self.work_type_selected, Message::WorkTypeChoice);
+        let type_classwork = Radio::new(WorkType::Classwork, "Классная работа", self.work_type_selected, Message::WorkTypeChoice);
+        let type_test1 = Radio::new(WorkType::Test1, "Самостоятельная работа", self.work_type_selected, Message::WorkTypeChoice);
+        let type_test2 = Radio::new(WorkType::Test2, "Проверочная работа", self.work_type_selected, Message::WorkTypeChoice);
+        let type_vocal_exam = Radio::new(WorkType::VocalExam, "Зачёт", self.work_type_selected, Message::WorkTypeChoice);
+        let type_exam = Radio::new(WorkType::Exam, "Контрольная работа", self.work_type_selected, Message::WorkTypeChoice);
 
         let layout = Row::new()
             .push(Column::new()
                   .push(grade_in).push(avg_out)
-                  .spacing(10).padding(10).width(FillPortion(9)))
+                  .spacing(10).padding(10).width(FillPortion(3)))
             .push(Column::new()
                   .push(type_classwork).push(type_test1)
-                  .push(type_test2).push(type_exam)
-                  .spacing(10).padding(10).width(FillPortion(3)))
+                  .push(type_test2).push(type_vocal_exam)
+                  .push(type_exam)
+                  .spacing(10).padding(10).width(FillPortion(5)))
             .push(Column::new()
                   .push(submit_button).push(rm_button)
                   .spacing(10).padding(10).width(FillPortion(5)))
